1cb1594c4c932f74ab57d518c3a63da31d403087
Prashant Kulkarni <prashantkul366@gmail.com>
2025-11-13 19:42:29 +0530

    Mobile mamba

--- networks/model_reg.py
+++ networks/model_reg.py
@@ -3,11 +3,16 @@ import importlib
 
 import torch
 import torch.nn as nn
-from timm.models._registry import is_model_in_modules
-from timm.models._helpers import load_checkpoint
-from timm.models.layers import set_layer_config
-from timm.models._hub import load_model_config_from_hf
-from timm.models._factory import parse_model_name
+# from timm.models._registry import is_model_in_modules
+from timm.models import is_model_in_modules
+# from timm.models._helpers import load_checkpoint
+from timm.models import load_checkpoint
+# from timm.models.layers import set_layer_config
+from timm.layers import set_layer_config
+# from timm.models._hub import load_model_config_from_hf
+from timm.models import load_model_config_from_hf
+# from timm.models._factory import parse_model_name
+from timm.models import parse_model_name
 # from util.registry import Registry
 
 
